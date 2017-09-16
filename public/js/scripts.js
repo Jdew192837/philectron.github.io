a550198171bfb375eb8496c9cb3cf7792535777a
Phi Luu <philectron@users.noreply.github.com>
2017-09-15 19:49:25 -0700

    Complete the implementation of Copy Code button

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,7 +8,9 @@ function copyCode() {
   $('pre').hover(
     // display copy code button when hover in
     function() {
-      // append div
+      // remember this <pre> only
+      var $pre = $(this);
+      // append container and button
       $(this).prepend(
         '<div class="copy-code-container"><div class="copy-code-button">Copy Code</div></div>'
       );
@@ -16,20 +18,35 @@ function copyCode() {
       $('.copy-code-button').css('color', $('.pln').css('color'));
       // on-click = copy the entire code snippet
       $('.copy-code-button').click(function() {
-        copyToClipboard('.linenums');
-        $(this).html('Copied!');
+        // store each line of code without html tags
+        var lines = '';
+        // iterate through all lists of .linenums inside of this
+        $pre.find('.linenums li').each(function() {
+          // add each line's text and an end-of-line character to the string
+          lines += $(this).text() + '\n';
+        });
+        // create a temporary div for storing lines
+        $pre.children('.linenums').append(
+          '<div id="temp-raw-code">' + lines + '</div>'
+        );
+        // execute copy command
+        copyToClipboard('#temp-raw-code');
+        // remove temporary tag created above
+        $('#temp-raw-code').remove();
+        // show that the code has been copied
+        $(this).html('Code Copied!');
       });
     },
     // remove copy code button when hover out
     function() {
-      $(this).find('.copy-code-container').remove();
+      $('.copy-code-container').remove();
     }
   );
 }
 
 // copy text to clipboard
 function copyToClipboard(element) {
-  var $temp = $('<input>');
+  var $temp = $('<textarea>');
   $('body').append($temp);
   $temp.val($(element).text()).select();
   document.execCommand('copy');
